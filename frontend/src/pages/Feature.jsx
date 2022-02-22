5efe3ada18311dbca64a2b872eb1cf43ce24c494
Martin Kvalsund <makv@stud.ntnu.no>
2022-02-22 15:43:49 +0100

    setup react project and navbar

---
+++ frontend/src/pages/Feature.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Feature = () => {
+    return (
+        <div
+            style={{
+                display: 'flex',
+                justifyContent: 'Right',
+                alignItems: 'Right',
+                height: '100vh'
+            }}
+        >
+            <h1>Feature</h1>
+        </div>
+    );
+};
+
+export default Feature;
